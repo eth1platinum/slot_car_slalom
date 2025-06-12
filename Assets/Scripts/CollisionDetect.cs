60fffff621481fefd9376691671b4118bad53550
Ethan Duckett <ethan.duckett1@gmail.com>
2025-06-12 09:15:36 +0100

    tidy sceneloader to collision integration

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -10,6 +10,7 @@ public class CollisionDetect : MonoBehaviour
     [SerializeField] AudioSource collisionFX;
     [SerializeField] GameObject mainCam;
     [SerializeField] GameObject fadeOut;
+    [SerializeField] SceneLoader loader;
 
     void OnTriggerEnter(Collider other)
     {
@@ -26,7 +27,6 @@ public class CollisionDetect : MonoBehaviour
         yield return new WaitForSeconds(1);
         fadeOut.SetActive(true);
         yield return new WaitForSeconds(3);
-        SceneLoader loader = new SceneLoader(); // todo maybe make this singleton?
         loader.loadMainMenu();
     }
 }
